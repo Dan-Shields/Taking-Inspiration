c193c4ecdb3e7a5ca4503601b8865696882602a9
Tom M <tom@collider.in>
2021-10-23 17:56:51 +0100

    Add WalkPath script

---
+++ Assets/Scripts/entities/WalkPath.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkPath : MonoBehaviour
+{
+    public List<Vector2> points;
+
+    private Vector2 direction;
+    private int index = 0;
+    private float segmentLength;
+
+    void Start()
+    {
+        this.StartSegment();
+    }
+
+    Vector2 Point(int i)
+    {
+        return this.points[i % this.points.Count];
+    }
+
+    Vector2 Position()
+    {
+        return (Vector2)this.transform.position;
+    }
+
+    void StartSegment()
+    {
+        this.direction = (this.Point(this.index + 1) - this.Point(this.index)).normalized;
+        this.segmentLength = (this.Point(this.index + 1) - this.Point(this.index)).magnitude;
+        this.transform.position = (Vector3) this.Point(this.index);
+    }
+
+    void FixedUpdate()
+    {
+        float distance = (this.Point(this.index) - (Vector2) this.transform.position).magnitude;
+        if (distance >= this.segmentLength)
+        {
+            this.index += 1;
+            this.StartSegment();
+        }
+        this.transform.position += (Vector3) this.direction * 0.2f * Time.fixedDeltaTime;
+    }
+}
